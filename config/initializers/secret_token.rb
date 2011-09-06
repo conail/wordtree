c1969ff8078567123a00c6c4d05a7fa5702a94e5
Conail Stewart <mail@conail.net>
2011-09-06 11:32:20 -0400

    Initial import (Rails 3.1.0).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrototypeThewordtreeNet::Application.config.secret_token = 'b3739a06777dcb388e57d88edb5319b953041a965c04e8a7b2b52768f7bcb361efd7aded9be4efc1c5a3fbce2ff157d5b7c8402fcfe8be4a00fbbbf82baee141'
