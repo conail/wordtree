4f8b75ea9040b03d4325393299e9f8ff30597ab1
Conail Stewart <conail@conail.net>
2012-01-12 21:45:15 +0000

    Added pre-IE9 fix for d3.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,13 @@
 //= require jquery_ujs
 //= require terms
 //= #require_tree .
+
+CSSStyleDeclaration.prototype.getProperty = function(a) {
+    return this.getAttribute(a);
+};
+CSSStyleDeclaration.prototype.setProperty = function(a,b) {
+    return this.setAttribute(a,b);
+}
+CSSStyleDeclaration.prototype.removeProperty = function(a) {
+    return this.removeAttribute(a);
+}
