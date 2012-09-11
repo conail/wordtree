af9bca8abaaf476ba2a5b956f0d891bc59093eca
Conail Stewart <conailstewart@gmail.com>
2012-09-11 11:02:06 -0400

    Remove unused models.

--- app/controllers/nodes_controller.rb
+++
@@ -1,6 +0,0 @@
-class NodesController < ApplicationController
-  def show
-	  @node = Node.new(id: params[:id])
-	  render json: @node.children.map(&:to_json)
-  end
-end
--- app/models/remainder.rb
+++
@@ -1,3 +0,0 @@
-class Remainder < ActiveRecord::Base
-  #
-end
